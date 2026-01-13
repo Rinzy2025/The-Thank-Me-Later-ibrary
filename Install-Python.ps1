20baebd952a6d76a98cd1b3fa43f905d034c4b96
Rinzy2025 <rinzymaster1@gmail.com>
2026-01-13 18:05:18 +0100

    Delete Install-Python.ps1

--- Install-Python.ps1
+++
@@ -1,5 +0,0 @@
-# Run uninstall script first
-powershell.exe -ExecutionPolicy Bypass -File ".\Uninstall-OldPython.ps1"
-
-# Install new Python silently
-Start-Process ".\python-3.14.2-amd64.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1 Include_launcher=1" -Wait
